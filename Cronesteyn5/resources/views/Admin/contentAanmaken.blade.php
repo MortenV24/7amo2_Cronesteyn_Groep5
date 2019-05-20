846d016227048c635224ef48aa3003356d61c9e0
yustin <yustintroost@gmail.com>
2019-05-20 09:53:29 +0200

    Routes Vogelwerkgroep & login

---
+++ Cronesteyn5/resources/views/Admin/contentAanmaken.blade.php
@@ -0,0 +1,9 @@
+@extends('Layouts/mainLayout')
+
+@section('title', 'Content maken')
+
+<div class="container col-md-8">
+    <div class="row">
+        @include('Includes/ContentAanmaakForm')
+    </div>
+</div>
