1adef091bc821837ee2fd4c489849deac05b3984
Morten <85415@mborijnland.nl>
2019-05-20 11:01:22 +0200

    Vogelwerkgroep & Login Routing

---
+++ Cronesteyn5/resources/views/login.blade.php
@@ -0,0 +1,7 @@
+@extends('Layouts/mainLayout')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Login</h1>
+@endsection
---
+++ Cronesteyn5/resources/views/vogelwerkgroep.blade.php
@@ -0,0 +1,7 @@
+@extends('Layouts/mainLayout')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Vogelwerkgroep</h1>
+@endsection
