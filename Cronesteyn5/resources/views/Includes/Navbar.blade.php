d80cf79d380817c4927a8a584afd82203a703dde
Morten <85415@mborijnland.nl>
2019-05-21 10:55:37 +0200

    Test

--- Cronesteyn5/resources/views/Includes/Navbar.blade.php
+++ Cronesteyn5/resources/views/Includes/Navbar.blade.php
@@ -16,17 +16,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ url('/admin/contentAanmaken') }}">content maken</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ url('/admin/contentAanmaken') }}">homo maken</a>
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link" href="{{ url('/admin/contentAanmaken') }}">homo 2</a>
-
-                    <a class="nav-link" href="{{ url('/admin/contentAanmaken') }}">nieps maken</a>
-
-                </li>
             </ul>
         </div>
 </nav>
