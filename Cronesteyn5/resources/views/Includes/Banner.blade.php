9e3d03f030b1b2b56f21a140752e8e145d4c2bb8
yustin <yustintroost@gmail.com>
2019-05-23 15:14:27 +0200

    kut foto

--- Cronesteyn5/resources/views/Includes/Banner.blade.php
+++ Cronesteyn5/resources/views/Includes/Banner.blade.php
@@ -1,9 +1,14 @@
 
 <div class=" banner">
     <h1 id="BannerTekst">@yield('BannerTekst')</h1>
+    <?php
+    $getFoto = DB::select('SELECT foto FROM contents WHERE title = "banner foto 1" ');
+    echo $getFoto;
+
+
+    ?>
 @foreach ($fotos as $foto)
     <img src="{{ URL::to('/img/' . $foto->foto) }}" alt="{{ $foto->title }}" id="BannerFoto" />
-
 @endforeach
 
 </div>
