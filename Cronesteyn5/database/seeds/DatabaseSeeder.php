c2282e46185626f0e6d5ad058351a08ffe8be14b
Yaimo Collins <1040905@mborijnland.nl>
2019-05-23 11:36:56 +0200

    yustin zn shit gemaakt

--- Cronesteyn5/database/seeds/DatabaseSeeder.php
+++ Cronesteyn5/database/seeds/DatabaseSeeder.php
@@ -15,11 +15,13 @@ class DatabaseSeeder extends Seeder
         DB::table('contents')->insert([
             'title' => 'banner foto 1',
             'foto' => 'forest-grass-green.jpg',
+            'text'=> false,
             'type' => 'foto'
         ]);
 
         DB::table('contents')->insert([
             'title' => 'vereniging vrienden polderpark cronesteyn',
+            'foto'=> false,
             'text' => 'Polderpark Cronesteyn is met 86 ha het grootste park in Leiden. Het bestaat nog relatief kort.In 1981 is het ontworpen door landschapsarchitect Evert Cornet (zie foto) van de gemeente Leiden. Sindsdien is het in 35 jaar een volwassen park geworden. Het park kent veel verschillende typen leefgebied, natuurlijk veel polder met weilanden en sloten, maar ook jonge bospercelen en struiken, een reigerkolonie die zetelt op de elzen van voormalige kwekerijen, grienden, een moerastuin en bos met resten van een oud landgoed. Door al die verschillende biotopen kent het de grootste soortenrijkdom van alle Leidse parken.',
             'type' => 'Parkvereniging'
         ]);
