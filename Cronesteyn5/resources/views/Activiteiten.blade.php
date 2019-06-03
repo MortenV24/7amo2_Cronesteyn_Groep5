cb5cccca30c839704090a16e44d565be54c1250d
Yaimo Collins <1040905@mborijnland.nl>
2019-06-03 15:27:11 +0200

    Banner en news include toegevoegd en gestyled

--- Cronesteyn5/resources/views/Activiteiten.blade.php
+++ Cronesteyn5/resources/views/Activiteiten.blade.php
@@ -1,15 +1,23 @@
 @extends('Layouts/mainLayout')
 
 @section('title', 'Activiteiten')
+@section('BannerTekst', 'Activiteiten')
 
 @section('content')
-    <h1>Activiteiten</h1>
+
+    @include('Includes/Banner')
 
     @foreach ($contents as $content)
-        <div id="activiteit">
-            <h1>{{$content->title}}</h1>
-            <p>{{$content->text}}</p>
-            <img src="{{ URL::to('/img/' . $content->foto) }}" alt="{{ $content->title }}" id="haasJong"/>
+        <div class="container">
+            <div class="col-md-6 row">
+                <h1>{{$content->title}}</h1>
+                <p>{{$content->text}}</p>
+                <img src="{{ URL::to('/img/' . $content->foto) }}" alt="{{ $content->title }}" id="haasJong"/>
+            </div>
+
+            <div class="col-md-6 row">
+                @include('Includes/newsItems')
+            </div>
         </div>
     @endforeach
 
--- Cronesteyn5/resources/views/Includes/Banner.blade.php
+++ Cronesteyn5/resources/views/Includes/Banner.blade.php
@@ -1,14 +1,9 @@
 
 <div class=" banner">
     <h1 id="BannerTekst">@yield('BannerTekst')</h1>
-    <?php
-//    $getFoto = DB::select('SELECT foto FROM contents WHERE title = "banner foto 1" ');
-//    echo $getFoto;
 
-
-    ?>
-@foreach ($Bannerfoto as $Bannerfoto)
-    <img src="{{ URL::to('/img/' . $Bannerfoto->foto) }}" alt="{{ $Bannerfoto->title }}" id="BannerFoto" />
+    @foreach ($Bannerfoto as $Bannerfoto)
+        <img src="{{ URL::to('/img/' . $Bannerfoto->foto) }}" alt="{{ $Bannerfoto->title }}" id="BannerFoto"/>
     @endforeach
 
 </div>
